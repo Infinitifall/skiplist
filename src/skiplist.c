d650a9be08b79bf3b992f8b214bafb78c380647d
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2021-10-23 11:27:15 +0530

    Changed malloc to calloc, improved code style for easier debugging

--- src/skiplist.c
+++ src/skiplist.c
@@ -3,22 +3,25 @@
 
 #include "skiplist.h"
 
-#define CL_STARTER_HEIGHT 10
+#define CL_STARTER_HEIGHT 4
 
 
 int generate_height() {
     int height = 1;
-    while (rand() % 2 == 0) { height += 1; }
+    while (rand() % 2 == 0) {
+        height += 1;
+    }
+
     return height;
 }
 
 
 Node* create_node(int data) {
-    Node *n = malloc(sizeof(Node));
+    Node *n = calloc(1, sizeof(Node));
     n->data = data;
     n->height = generate_height();
-    n->succ_array = malloc(n->height * sizeof(Node*));
-    n->pred_array = malloc(n->height * sizeof(Node*));
+    n->succ_array = calloc(n->height, sizeof(Node*));
+    n->pred_array = calloc(n->height, sizeof(Node*));
     return n;
 }
 
@@ -31,9 +34,9 @@ void free_node(Node *n) {
 
 
 SkipList* create_list() {
-    SkipList *l = malloc(sizeof(SkipList));
+    SkipList *l = calloc(1, sizeof(SkipList));
     // create head_array of size CL_STARTER_HEIGHT which we can expand later
-    l->head_array = malloc(CL_STARTER_HEIGHT * sizeof(Node*));
+    l->head_array = calloc(CL_STARTER_HEIGHT, sizeof(Node*));
     l->max_height = CL_STARTER_HEIGHT;
     return l;
 }
@@ -46,7 +49,7 @@ void free_list(SkipList *l) {
         free_node(curr);
         curr = next;
     }
-    
+
     free(l->head_array);
     free(l);
 }
@@ -65,7 +68,10 @@ void list_print_pretty(SkipList *l) {
     Node *curr = l->head_array[0];
     while(curr != NULL) {
         printf("%20d, ", curr->data);
-        for (int i = 0; i < curr->height; i++) { printf("_"); }
+        for (int i = 0; i < curr->height; i++) {
+            printf("_");
+        }
+
         printf("\n");
         curr = curr->succ_array[0];
     }
@@ -77,21 +83,38 @@ Node* list_search(SkipList *l, int data) {
     int curr_height = l->max_height;
     while(curr_height >= 0) {
         Node *next = l->head_array[curr_height];
-        if (next == NULL) { curr_height -=1; }
-        else if (next->data > data) { curr_height -=1; }
-        else if (next->data == data) { return next; }
-        else { break; }
+        if (next == NULL) {
+            curr_height -=1;
+
+        } else if (next->data > data) {
+            curr_height -=1;
+
+        } else if (next->data == data) {
+            return next;
+
+        } else {
+            break;
+        }
     }
     
     // traverse the skip list going down or forward
     Node *curr = l->head_array[curr_height];
     while(curr_height >= 0) {
         Node *next = curr->succ_array[curr_height];
-        if (next == NULL) { curr_height -= 1; }
-        else if (next->data > data) { curr_height -= 1; }
-        else if (next->data == data) { return next; }
-        else { curr = next; }
+        if (next == NULL) {
+            curr_height -= 1;
+
+        } else if (next->data > data) {
+            curr_height -= 1;
+
+        } else if (next->data == data) {
+            return next;
+
+        } else {
+            curr = next;
+        }
     }
+
     return NULL;
 }
 
@@ -99,10 +122,11 @@ Node* list_search(SkipList *l, int data) {
 void list_insert(SkipList *l, int data) {
     Node *n = create_node(data);
     
-    // double size of head_array if necessary
+    // increase size of head_array by a set amount if necessary (doubling size is unnecessary
+    // because height is not expected to exceed log(n))
     while (l->max_height < n->height) {
-        l->head_array = realloc(l->head_array, 2 * l->max_height * sizeof(*l->head_array));
-        l->max_height *= 2;
+        l->head_array = realloc(l->head_array, CL_STARTER_HEIGHT * sizeof(Node*) +  sizeof(*l->head_array));
+        l->max_height += CL_STARTER_HEIGHT;
     }
 
     // check levels in head_array from top to bottom
@@ -110,7 +134,11 @@ void list_insert(SkipList *l, int data) {
     while(curr_height >= 0) {
         Node *next = l->head_array[curr_height];
         if (next == NULL) {
-            if (n->height - 1 >= curr_height) { l->head_array[curr_height] = n; }
+            if (n->height - 1 >= curr_height) {
+                l->head_array[curr_height] = n;
+                printf("%p\n", l->head_array[curr_height]);
+            }
+
             curr_height -=1;
 
         } else if (next->data > data) {
@@ -119,9 +147,12 @@ void list_insert(SkipList *l, int data) {
                 next->pred_array[curr_height] = n;
                 l->head_array[curr_height] = n;
             }
+
             curr_height -=1;
 
-        } else { break; }
+        } else {
+            break;
+        }
     }
 
     // traverse the skip list going down or forward
@@ -133,6 +164,7 @@ void list_insert(SkipList *l, int data) {
                 curr->succ_array[curr_height] = n;
                 n->pred_array[curr_height] = curr;
             }
+
             curr_height -= 1;
 
         } else if (next->data > data) {
@@ -142,9 +174,11 @@ void list_insert(SkipList *l, int data) {
                 n->pred_array[curr_height] = curr;
                 curr->succ_array[curr_height] = n;
             }
+            
             curr_height -= 1;
-
-        } else { curr = next; }
+        } else {
+            curr = next;
+        }
     }
 }
 
@@ -153,7 +187,10 @@ void list_remove(SkipList *l, int data) {
     // get pointer to the element using list_search
     Node *curr = list_search(l, data);
 
-    if(curr == NULL) { return; }
+    if(curr == NULL) {
+        return;
+    }
+
     // set pointers of succ and pred on each level
     for (int i = 0; i <curr->height; i++) {
         Node *succ = curr->succ_array[i];
@@ -178,6 +215,7 @@ void list_remove(SkipList *l, int data) {
             }
         }
     }
+
     free_node(curr);
 }
 
